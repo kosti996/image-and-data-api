b724f340d45a9500df3ad0014ad2c70a1841f484
kosti996 <kostiukevycha@gmail.com>
2025-03-26 18:38:24 +0100

    added upload controller

---
+++ image-and-data-api/Controllers/UploadController.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class UploadController : ControllerBase
+{
+    private readonly string _blobConnectionString;
+    private readonly string _containerName;
+
+    public UploadController(IConfiguration configuration)
+    {
+        _blobConnectionString = configuration["ConnectionString"];
+        _containerName = configuration["ContainerName"];
+    }
+
+    [HttpPost(Name = "upload")]
+    public async Task<IActionResult> Upload([FromForm] IFormCollection form)
+    {
+        try
+        {
+            // Access form fields
+            var dropdown = form["dropdown"].ToString();
+            var textInput = form["textInput"].ToString();
+            var freeText = form["freeText"].ToString();
+
+            // Access the file
+            var file = form.Files["file"];
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            // Upload to Azure Blob
+            var blobClient = new BlobClient(_blobConnectionString, _containerName, file.FileName);
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+
+            return Ok(new { Message = "File uploaded successfully!" });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
---
+++ image-and-data-api/Program.cs
@@ -0,0 +1,44 @@
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAzureKeyVault(
+    new Uri("https://blob-images.vault.azure.net/"),
+    new DefaultAzureCredential());
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowAll"); // Apply the policy globally
+
+app.MapControllers();
+
+app.Run();
